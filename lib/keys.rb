844fc17ab0b31858fd7a7d91ecdb285214f1f13d
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 15:44:04 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,5 +9,5 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries = {dairy: ["milk", "yogurt", "cheese", "carrots", "broccoli", "cucumbers", "chicken", "steak", "salmon", "rice", "pasta"]
 end
